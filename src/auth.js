0f057964be8c7306f13069ebadd532cf666ba593
Joe Nudell <joen@dropbox.com>
2016-12-26 22:48:46 -0500

    add auth for pages

--- src/auth.js
+++ src/auth.js
@@ -1,12 +1,20 @@
 export default {
     views: {
-        posts: false
+        posts: false,
+        pages: false
     },
     update: [
         {
             match: {
                 field: 'type',
                 value: 'post'
+            },
+            auth: true
+        },
+        {
+            match: {
+                field: 'type',
+                value: 'page'
             },
             auth: true
         }
